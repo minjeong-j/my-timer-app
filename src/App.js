49f843e573f9c4e5dba8576b143123075d1d7712
minjeong-j <yulalswjdj11@gmail.com>
2024-11-26 15:27:12 +0900

    react timer 컴포넌트 제거(언마운트) 실행

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Timer from "./Timer";
+
+// 타이머 정리가 제대로 작동하는지 확인
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/timer">Timer</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/timer" element={<Timer />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+/*
+import React from "react";
 import Timer from "./Timer";
 
 function App() {
@@ -11,3 +33,4 @@ function App() {
 }
 
 export default App;
+*/
--- src/Timer.js
+++ src/Timer.js
@@ -10,7 +10,10 @@ function Timer() {
         	setSeconds((prev) => prev +1);	//1초마다 seconds 상태 업데이트
         }, 1000);
         
-        return () => clearInterval(interval);	//componentWillUnmount와 동일한 역할
+        return () => {
+            console.log("componentWillUnmount와 동일한 역할");
+            clearInterval(interval);	//componentWillUnmount와 동일한 역할
+        }    
     }, []);	//빈 배열로 의존성 없음 => 마운트/언마운트 시 실행
     
     return <p>Seconds: {seconds}</p>;
